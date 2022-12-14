048a908cb041a54e0e5ae6846d580079cd6bef22
chezapangestu <cheza426@gmail.com>
2022-12-14 11:51:56 +0700

    first commit

---
+++ src/AddToDo.svelte
@@ -0,0 +1,26 @@
+<script>
+
+  let value = "";
+  export let toDos;
+  async function handleClick() {
+
+    toDos.push({ data: { title: value }, ref: { value: { id: Math.random() } } });
+    toDos = toDos;
+    value = "";
+  }
+
+  import "./app.css";
+</script>
+
+<form class="todo-wrapper" on:submit|preventDefault={handleClick}>
+  <div>
+    <input class="input-list" required placeholder="Input new" bind:value={value}/>
+  </div>
+  <button class="submit-button" type="submit" >
+    + Add
+  </button>
+</form>
+
+<style lang="css">
+
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import "./app.css";
+  import AddToDo from "./AddToDo.svelte";
+
+  let toDos = [
+    { data: { title: "Go to..."}, ref: { value: { id: "1" } } },
+    { data: { title: "Breakfast"} }
+  ];
+  async function handelClick(ref) {
+    toDos = toDos.filter(todo => todo.ref !== ref);
+  }
+</script>
+
+<main>
+  <h1>To-Do List</h1>
+  <div class="">
+    <AddToDo bind:toDos={toDos}/>
+    <div class="">
+      <h3>Today's list:</h3>
+      <ul>
+        {#each toDos as toDo}
+          <li>
+              <label>
+                <div class="checkbox"> 
+                  <input type="checkbox" name="packersOff" value="1"> 
+                  <label class="strikethrough">{toDo?.data?.title}</label> 
+                </div>
+                <button class="remove-button" on:click={() => handelClick(toDo.ref)}>remove</button>
+                <hr>
+              </label>
+              
+            
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</main>
+
+<!--maiza radhiya marjjonzzzzzzzzzzzzzzz-->
